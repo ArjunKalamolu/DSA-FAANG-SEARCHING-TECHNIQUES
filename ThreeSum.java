7733d1c57e90e533dd425f129a3fa9199f29cc6d
Arjun Kumar <kalamoluarjunkumar@gmail.com>
2026-05-26 15:57:25 +0530

    3Sum Java

---
+++ ThreeSum.java
@@ -0,0 +1,36 @@
+import java.util.*;
+public class ThreeSum {
+    public static void main(String[] args){
+        Scanner input = new Scanner(System.in);
+        int n = input.nextInt();
+        int[] nums = new int[n];
+        for(int i=0; i<nums.length; i++){
+            nums[i] = input.nextInt();
+        }
+        System.out.println(threeSum(nums));
+
+    }
+    static List<List<Integer>> threeSum(int[] nums){
+        List<List<Integer>> res = new ArrayList<>();
+        Arrays.sort(nums);
+        for(int i=0; i<nums.length - 2; i++){
+            if(i > 0 && nums[i] == nums[i-1]) continue;
+            int l = i+1; int r = nums.length - 1;
+            while(l < r){
+                int sum = nums[i]+nums[l]+nums[r];
+                if(sum == 0){
+                    res.add(Arrays.asList(nums[i],nums[l],nums[r]));
+                    while(l < r && nums[l] == nums[l+1]) l++;
+                    while(l < r && nums[r] == nums[r-1]) r--;
+                    l++;
+                    r--;
+                }else if(sum > 0){
+                    r--;
+                }else{
+                    l++;
+                }
+            }
+        }
+        return res;
+    }
+}
